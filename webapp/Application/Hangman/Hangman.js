53d411e02349eb848f9e519bc1bcd6a3bf5a79a2
andras2409 <andras.nagy2002@gmail.com>
2023-05-25 17:50:36 +0200

    hangman dom started

--- webapp/Application/Hangman/Hangman.js
+++ webapp/Application/Hangman/Hangman.js
@@ -1,227 +1,53 @@
-const letterContainer = document.getElementById("letter-container");
-const optionsContainer = document.getElementById("options-container");
-const userInputSection = document.getElementById("user-input-section");
-const newGameContainer = document.getElementById("new-game-container");
-const newGameButton = document.getElementById("new-game-button");
-const canvas = document.getElementById("canvas");
-const resultText = document.getElementById("result-text");
+import Application from "../Application.js";
 
-let options = {
-  
-  Állatok: [
-    "Kutya", 
-    "Macska", 
-    "Tigris", 
-    "Panda", 
-    "Medve", 
-    "Zebra"
-    ],
-    Gyümölcsök: [
-        "Alma",
-        "Szilva",
-        "Barack",
-        "Eper",
-        "Dinnye",
-        "Paradicsom"
-        ],
-  Autók: [
-    "Suzuki",
-    "Audi",
-    "Honda",
-    "Mercedes",
-    "Lexus",
-    "Pontiac"
-  ],
-};
+export default class Chess extends Application{
 
-let winCount = 0;
-let count = 0;
-
-let chosenWord = "";
-
-const displayOptions = () => {
-  optionsContainer.innerHTML += `<h3>Válasszon kategóriát</h3>`;
-  let buttonCon = document.createElement("div");
-  for (let value in options) {
-    buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`;
-  }
-  optionsContainer.appendChild(buttonCon);
-};
-
-//Gombok tiltása
-const blocker = () => {
-  let optionsButtons = document.querySelectorAll(".options");
-  let letterButtons = document.querySelectorAll(".letters");
-  //kategóriák tiltása
-  optionsButtons.forEach((button) => {
-    button.disabled = true;
-  });
-
-  //betűk tiltása
-  letterButtons.forEach((button) => {
-    button.disabled.true;
-  });
-  newGameContainer.classList.remove("hide");
-};
-
-//Szó generáló
-const generateWord = (optionValue) => {
-  let optionsButtons = document.querySelectorAll(".options");
-
-  //Belső gomb kijelölése kiválasztásra
-  optionsButtons.forEach((button) => {
-    if (button.innerText.toLowerCase() === optionValue) {
-      button.classList.add("active");
+    init(){
+        this.initDOM();
     }
-    button.disabled = true;
-  });
 
-  //korábbi szó, betűk törlése
-  letterContainer.classList.remove("hide");
-  userInputSection.innerText = "";
+    initDOM(){
+        const gameContainer = document.createElement('div');
+        gameContainer.id = 'hangman-container';
+        this.app.appendChild(gameContainer);
 
-  let optionArray = options[optionValue];
+        gameContainer.appendChild(document.createElement('div'));
+        gameContainer.lastChild.id = 'option-container';
 
-  //random szó kiválasztása
-  chosenWord = optionArray[Math.floor(Math.random() * optionArray.length)];
-  chosenWord = chosenWord.toUpperCase();
-  console.log(chosenWord);
+        gameContainer.lastChild.appendChild(document.createElement('h3'));
+        gameContainer.lastChild.lastChild.textContent = 'Choose a category';
 
-  //gondolatjelek a betűk helyére
-  let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
-  userInputSection.innerHTML = displayItem;
-};
+        gameContainer.lastChild.appendChild(document.createElement('div'));
 
-//Kezdeti megjelenés
-const initializer = () => {
-  winCount = 0;
-  count = 0;
+        gameContainer.lastChild.lastChild.appendChild(document.createElement('button'));
+        gameContainer.lastChild.lastChild.lastChild.textContent = 'Animals';
+        gameContainer.lastChild.lastChild.appendChild(document.createElement('button'));
+        gameContainer.lastChild.lastChild.lastChild.textContent = 'Fruits';
+        gameContainer.lastChild.lastChild.appendChild(document.createElement('button'));
+        gameContainer.lastChild.lastChild.lastChild.textContent = 'Cars';
 
-  userInputSection.innerHTML = "";
-  optionsContainer.innerHTML = "";
-  letterContainer.classList.add("hide");
-  newGameContainer.classList.add("hide");
-  letterContainer.innerHTML = "";
+        gameContainer.appendChild(document.createElement('div'));
+        gameContainer.lastChild.id = 'letter-container';
 
-  //betűk létrehozása
-  for (let i = 65; i < 91; i++) {
-    let button = document.createElement("button");
-    button.classList.add("letters");
-    //Szám -> ASCII[A-Z]
-    button.innerText = String.fromCharCode(i);
-    button.addEventListener("click", () => {
-      let charArray = chosenWord.split("");
-      let dashes = document.getElementsByClassName("dashes");
-      //kötőjel kicserélése betűre
-      if (charArray.includes(button.innerText)) {
-        charArray.forEach((char, index) => {
-          //Ha egyezik
-          if (char === button.innerText) {
-            dashes[index].innerText = char;
-            winCount += 1;
-            if (winCount == charArray.length) {
-              resultText.innerHTML = `<h2 class='win-msg'>Győztél!!</h2><p>A szó a <span>${chosenWord}</span> volt.</p>`;
-              blocker();
-            }
-          }
-        });
-      } else {
-        //sikertelen tipp
-        count += 1;
-        drawMan(count);
-        if (count == 6) {
-          resultText.innerHTML = `<h2 class='lose-msg'>Vesztettél!!</h2><p>A szó a <span>${chosenWord}</span> volt.</p>`;
-          blocker();
+        for(let i = 65; i < 91; i++){
+            gameContainer.lastChild.appendChild(document.createElement('button'));
+            gameContainer.lastChild.lastChild.classList.add("letter");
+            gameContainer.lastChild.lastChild.textContent = String.fromCharCode(i);
         }
-      }
-      button.disabled = true;
-    });
-    letterContainer.append(button);
-  }
 
-  displayOptions();
-  let { initialDrawing } = canvasCreator();
-  initialDrawing();
-};
+        gameContainer.appendChild(document.createElement('div'));
+        gameContainer.lastChild.id = 'user-input-section';
 
-//Akasztófa felülete
-const canvasCreator = () => {
-  let context = canvas.getContext("2d");
-  context.beginPath();
-  context.strokeStyle = "#000";
-  context.lineWidth = 2;
+        this.canvas = new Canvas();
+    }
+}
 
-  const drawLine = (fromX, fromY, toX, toY) => {
-    context.moveTo(fromX, fromY);
-    context.lineTo(toX, toY);
-    context.stroke();
-  };
+class Canvas{
+    constructor(){
+        this.initCanvas();
+    }
 
-  const head = () => {
-    context.beginPath();
-    context.arc(70, 30, 10, 0, Math.PI * 2, true);
-    context.stroke();
-  };
-
-  const body = () => {
-    drawLine(70, 40, 70, 80);
-  };
-
-  const leftArm = () => {
-    drawLine(70, 50, 50, 70);
-  };
-
-  const rightArm = () => {
-    drawLine(70, 50, 90, 70);
-  };
-
-  const leftLeg = () => {
-    drawLine(70, 80, 50, 110);
-  };
-
-  const rightLeg = () => {
-    drawLine(70, 80, 90, 110);
-  };
-
-  //Keret
-  const initialDrawing = () => {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    drawLine(10, 130, 130, 130);
-    drawLine(10, 10, 10, 131);
-    drawLine(10, 10, 70, 10);
-    drawLine(70, 10, 70, 20);
-  };
-
-  return { initialDrawing, head, body, leftArm, rightArm, leftLeg, rightLeg };
-};
-
-//testrészek berajzolása
-const drawMan = (count) => {
-  let { head, body, leftArm, rightArm, leftLeg, rightLeg } = canvasCreator();
-  switch (count) {
-    case 1:
-      head();
-      break;
-    case 2:
-      body();
-      break;
-    case 3:
-      leftArm();
-      break;
-    case 4:
-      rightArm();
-      break;
-    case 5:
-      leftLeg();
-      break;
-    case 6:
-      rightLeg();
-      break;
-    default:
-      break;
-  }
-};
-
-//Új játék
-newGameButton.addEventListener("click", initializer);
-window.onload = initializer;
+    initCanvas(){
+        console.log('initCanvas started');
+    }
+}
